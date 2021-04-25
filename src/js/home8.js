724f7dbc33f6b770a5bc88d1d5e842810fe1b3a4
Maksym Chervinskyi <66177888+Gaivir@users.noreply.github.com>
2021-04-25 07:03:14 +0300

    Дороблює ДЗ 8

---
+++ src/js/home8.js
@@ -0,0 +1,64 @@
+import images from "./gallery-items.js";
+
+
+const imgsContainerEl = document.querySelector('.js-gallery');
+const lightboxContainerEl = document.querySelector('.lightbox');
+const modalImgEl = document.querySelector('.lightbox__image');
+const origImgEl = document.querySelector('.gallery__image');
+const btnCloseEl = document.querySelector('.lightbox__button');
+
+
+const imgsMake = createImgsCards(images);
+imgsContainerEl.insertAdjacentHTML('beforeend', imgsMake);
+
+
+imgsContainerEl.addEventListener('click', onPaletteContainertClick);
+btnCloseEl.addEventListener('click', btnCloseClick);
+
+// Створюємо колекцію карток
+function createImgsCards(images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+ 
+    }).join(' ');
+}
+
+
+function onPaletteContainertClick(evt) {
+  evt.preventDefault();
+  const isImgSwatchEl = evt.target.classList.contains('gallery__image');
+ 
+    if (!isImgSwatchEl) {
+        return;
+  }
+  console.log(evt.target);
+  
+  lightboxContainerEl.classList.add("is-open");
+
+  
+  modalImgEl.setAttribute('src', origImgEl.getAttribute('data-source'));
+  
+}
+
+// Кнопка
+function btnCloseClick() {
+  lightboxContainerEl.classList.remove("is-open");
+  modalImgEl.src = ' ';
+}
+
+
+
+
